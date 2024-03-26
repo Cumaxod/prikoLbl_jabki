066096f1583a47317b0dc1c559b92ddeea39991d
Cumaxod <Edvirdlo@gmail.com>
2024-03-26 15:50:19 +0300

    homevorke

--- src/src/credit/Na_zachet_3.java
+++ src/src/credit/Na_zachet_3.java
@@ -2,17 +2,18 @@ package credit;
 
 public class Na_zachet_3 {
     public static void main(String[] args) {
-        int n = (int) (Math.random() * 28800);
+        // int n = (int) (Math.random() * 28800);
+        int n = 0;
         System.out.println("до конца рабочего дня в секундах: " + n); // для Петрова
         int hours = n / 3600;
         int minutes = (n - hours * 3600) / 60;
         int seconds = (n - hours * 3600) % 60;
-        if (hours == 0){
-            System.out.println("Остался завершающий час работы");
-        }else{
-            System.out.println("до конца рабочего дня осталось столько часов");
-            System.out.println(hours);
+        if (n == 0){
+            System.out.println("рабочий день закончился");
+        } else if (hours == 0) {
+            System.out.println("Осталось менее часа,а именно:" + minutes + "мин. " + seconds + "сек.");
+        } else if (hours > 0) {
+            System.out.println("до конца рабочего дня: " + hours + "ч. " + minutes + "мин. " + seconds + "сек.");
         }
-        System.out.println("до конца рабочего дня: " + hours + "ч. " + minutes + "мин. " + seconds + "сек.");
     }
 }
